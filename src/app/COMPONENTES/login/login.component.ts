cc178375c44542b4a4f9bb3af9cd941731797b65
Cortez Aldo Gabriel <88732550+clementine777@users.noreply.github.com>
2023-01-12 18:16:19 -0300

    hasta donde llegue

--- src/app/COMPONENTES/login/login.component.ts
+++ src/app/COMPONENTES/login/login.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
